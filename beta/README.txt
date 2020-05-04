9000905472abb1cba4f344c91ff560fb49e77fa1
Ron Yeh <ronyeh@gmail.com>
2020-05-04 12:59:13 -0700

    Deleting everything from the publishing branch.

--- beta/README.txt
+++
@@ -1,1 +0,0 @@
-Use beta/index.html to distribute links for downloading IPAs or APKs of our beta software.
--- test.sh
+++
@@ -1,3 +0,0 @@
-open http://localhost:8123
-echo "Running at http://localhost:8123/"
-python -m SimpleHTTPServer 8123
